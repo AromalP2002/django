7e115b943a5f385895da46bd1eb198b953b92b21
AromalP2002 <vivekcherupoika20@gmail.com>
2024-11-15 00:50:17 +0530

    work done

--- bkmyshow/app/models.py
+++ bkmyshow/app/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 
-
 # Create your models here.
-class movie(models.Model):
+class Movie(models.Model):
     name=models.TextField()
     date=models.DateField()
-    frgrnd_img=models.FieldFile()
-    bckgrd_img=models.FieldFile()
+    frgrnd_img=models.FileField()
+    bckgrnd_img=models.FileField()
     lang=models.TextField()
     durtn=models.TextField()
     diamtn=models.TextField()
+    type=models.TextField()
+    cert=models.TextField()
+    about=models.TextField()
--- bkmyshow/app/views.py
+++ bkmyshow/app/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login,logout
+from .models import *
+import os
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 
 
 # Create your views here.
 def homepage(req):
     return render( req, 'homepage.html')
+def sec(req):
+    return render(req ,'sec.html')
+ 
